954efc4520a4936423f22f7fc35adab7b6cc9365
= <=>
2021-04-20 01:19:14 +0300

    Finished with queri

---
+++ api/api/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Проект</h1>
+      <h3>Напишите название команды</h3>
+      <input type="text" className="main-input"/>
+      <button className="main-button">Записаться!</button>
+    </div>
+  );
+}
+
+export default App;
